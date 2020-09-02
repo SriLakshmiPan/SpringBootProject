0311bc064a09531ded859db81fd10e9cb5c39146
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-09-01 23:00:49 -0400

    Employee details by id

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -7,50 +7,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.howtodoinjava.demo.model.request.Employee;
+import com.howtodoinjava.demo.model.request.EmployeeId;
+import com.howtodoinjava.demo.model.response.EmployeeDetailsResponse;
 
-import com.howtodoinjava.demo.model.Employee;
-@RequestMapping("/")//url 
-@RestController//Annotation to control the program 
+@RequestMapping("/sample") // url
+@RestController // Annotation to control the program
 public class EmployeeController {
+	List<Employee> employeesList = new ArrayList<Employee>();// List is to add all employee details
+	Employee emp = new Employee();
 
-	
-	List<Employee> employeesList = new ArrayList<Employee>();//List is to add all employee details
-
-//	@RequestMapping(method = RequestMethod.GET)//RequestMapping annotation to call get method
-//    public List<Employee> getEmployees() 
-//    {
-//		return employeesList;
-//    }
-//	@RequestMapping(method = RequestMethod.POST)//post method
-//    public String addEmployees() //addEmployees method
-//    {
-//		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
-//		return "Employee Added";
-//    }
-	 @PostMapping("/employees")
+	@PostMapping("/employees")
 	public String newEmployee(@RequestBody Employee newEmployee) {
-		 employeesList.add(newEmployee);
+		employeesList.add(newEmployee);
 		return "Employee Added";
-	 }
-	 @GetMapping("/employees")
-	 public List<Employee> getEmployees1() 
-//	 public String getEmployees1()
-	    {
-//		 String tt1 = null;
-			return employeesList;
-//		 for (int i = 0; i < employeesList.size(); i++) {
-//	            tt1= tt1 + employeesList.get(i).toString() + "\n";
-//	        }
-//		 return tt1;
-	    }
-//	 }
-	
-//	@PostMapping("/hello")
-//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-//		return String.format("Hello %s!", name);
-//	}
+	}
+
+	@GetMapping("/getEmployees")
+	public List<Employee> getEmployees1() {
+		return employeesList;
+	}
+
+	@PostMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/getEmployeeDetailsbyID")
+//	public Integer getEmployeeDetails (@RequestParam Integer empId) {
+	public Employee getEmployeeDetails(@RequestParam Integer empId) {
+		System.out.println("employeesList" + employeesList.toString());
+		for (Employee emp : employeesList) {
+			if (emp.getId() == empId) {
+				return emp;
+			}
+		}
+		return null;
+
+//		return empDetails;
+	}
+
 }
---
+++ src/main/java/com/howtodoinjava/demo/model/request/EmployeeId.java
@@ -0,0 +1,20 @@
+package com.howtodoinjava.demo.model.request;
+
+public class EmployeeId {
+//	
+//	private Integer id;
+//	
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//	
+//	@Override
+//	public String toString() {// returns a string representation of an object
+//		return "Employee [id=" + id + "]";
+//	}
+//}
+}
